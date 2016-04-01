a0ec4aae2d9b4e07f281d49235d11543ffb9066f
Victor Bjelkholm <victor@typeform.com>
2016-04-01 14:29:48 -0500

    Initialize config only if not exists

--- cli.js
+++ cli.js
@@ -1,10 +1,18 @@
 #! /usr/bin/env node
 
+const fs = require('fs')
 const args = process.argv
 const command = args[2]
 
 const config = {
   app_dir: process.env.HOME + '/.stay'
+}
+
+try {
+  fs.accessSync(config.app_dir, fs.F_OK)
+} catch (e) {
+  const mkdirp = require('mkdirp')
+  mkdirp.sync(config.app_dir)
 }
 
 if (!command) {
@@ -14,14 +22,10 @@ if (!command) {
 
 try {
   require('./cli/' + command)(args, config)
-} catch (err) {
-  console.log(err)
-  if (err.code === 'MODULE_NOT_FOUND') {
+} catch (Err) {
+  if (Err.code === 'MODULE_NOT_FOUND') {
     console.log('Command "' + command + '" not found')
   } else {
-    throw new Error(err)
+    throw Err
   }
 }
-
-const mkdirp = require('mkdirp')
-mkdirp.sync(config.app_dir)
